36fdd75b90b69a5ac72e3613e4fe9b7b63be400d
Creastv <creastv@gail.com>
2025-06-16 07:12:47 +0200

    all

--- templates-parts/header/header-brand.php
+++ templates-parts/header/header-brand.php
@@ -5,7 +5,7 @@ $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
 <div class="navbar__brand">
     <a href="<?php echo esc_url(home_url('/')); ?>">
         <?php if (has_custom_logo()) {
-            echo '<img src="' . esc_url($logo[0]) . '" alt="' . esc_attr(get_bloginfo('name')) . '">';
+            echo '<img width="229" height="45" src="' . esc_url($logo[0]) . '" alt="' . esc_attr(get_bloginfo('name')) . '">';
         } else {
             echo '<h1>' . esc_html(get_bloginfo('name')) . '</h1>';
         } ?>
